bbd65fea48b5c88003c910a831e8f3e3193d30f0
quratmanzoor662 <161446609+quratmanzoor662@users.noreply.github.com>
2024-02-27 15:31:01 +0530

    Add files via upload

---
+++ playfair.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void playfair(char str[],charkeystr[])
+{
+char matrix[5][5];
+char key=strlen(keystr);
+char plaintext=strlen(str);
+  void touppercase(charencrypt[],intps)
+{
+	for(int i=0;i<plaintext;i++)
+}
+
+}
